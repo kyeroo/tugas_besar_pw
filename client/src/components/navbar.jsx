73c9c6876aef8a7426ccde93d19f9d9edcec7ead
Choirur Rizky <rizkyfirdaus250607@gmail.com>
2026-05-08 19:23:25 +0800

    fix logout

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -78,6 +78,7 @@ export default function Navbar() {
           <button
             onClick={() => {
               localStorage.removeItem("token");
+              localStorage.removeItem("user");
               setIsLogin(false);
               navigate("/");
             }}
